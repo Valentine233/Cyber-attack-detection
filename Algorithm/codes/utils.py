28d13d3dafb1adb1060ab08ece2450b17577be6c
zhanjunlang <36733806+zhanjunlang@users.noreply.github.com>
2018-11-18 22:33:23 +0800

    Update utils.py
    fix the loading error

--- Algorithm/codes/utils.py
+++ Algorithm/codes/utils.py
@@ -20,14 +20,21 @@ def getSent(anor_test, nor_test, nor_train, request_file, label_file):
             for line in f:
                 labels.append(line.strip())
     else:
+        print("Creating request_file and label_file...")
         with open(anor_test,'r') as f:
             first_line = True
+            content_flag = False
             for line in f:
                 if first_line == True and line.strip() != "":
                     sentences.append(line.strip())
                     labels.append("1")
                     first_line = False
-                if line.strip() == "":
+                elif first_line == False and line.strip().startswith("Content-Length:"):
+                    content_flag = True
+                elif first_line == False and line.strip() != "" and content_flag == True:
+                    sentences[-1] = sentences[-1] + line.strip()
+                    content_flag = False
+                elif line.strip() == "" and content_flag == False:
                     first_line = True
         with open(nor_test,'r') as f:
             first_line = True
@@ -36,7 +43,12 @@ def getSent(anor_test, nor_test, nor_train, request_file, label_file):
                     sentences.append(line.strip())
                     labels.append("0")
                     first_line = False
-                if line.strip() == "":
+                elif first_line == False and line.strip().startswith("Content-Length:"):
+                    content_flag = True
+                elif first_line == False and line.strip() != "" and content_flag == True:
+                    sentences[-1] = sentences[-1] + line.strip()
+                    content_flag = False
+                elif line.strip() == "" and content_flag == False:
                     first_line = True
         with open(nor_train,'r') as f:
             first_line = True
@@ -45,10 +57,23 @@ def getSent(anor_test, nor_test, nor_train, request_file, label_file):
                     sentences.append(line.strip())
                     labels.append("0")
                     first_line = False
-                if line.strip() == "":
+                elif first_line == False and line.strip().startswith("Content-Length:"):
+                    content_flag = True
+                elif first_line == False and line.strip() != "" and content_flag == True:
+                    sentences[-1] = sentences[-1] + line.strip()
+                    content_flag = False
+                elif line.strip() == "" and content_flag == False:
                     first_line = True
         with open(request_file,'w') as f:
             f.write('\n'.join(sentences))
         with open(label_file,'w') as f:
             f.write('\n'.join(labels))
     return sentences, labels
+
+if __name__ == "__main__":
+    anor_test = '../data/HTTP_2010/anomalousTrafficTest.txt'
+    nor_test = '../data/HTTP_2010/normalTrafficTest.txt'
+    nor_train = '../data/HTTP_2010/normalTrafficTraining.txt'
+    request_file = '../data/HTTP_2010/request.txt'
+    label_file = '../data/HTTP_2010/label.txt'
+    sentences, labels = getSent(anor_test, nor_test, nor_train, request_file, label_file)
