5a63bda0648a7d6dc853533ee02c24a63994efd8
zhanjunlang <36733806+zhanjunlang@users.noreply.github.com>
2018-12-13 21:59:19 +0800

    Update train_lm.py

--- Algorithm/codes/tmp/train_lm.py
+++ Algorithm/codes/tmp/train_lm.py
@@ -32,7 +32,7 @@ device = torch.device("cuda" if USE_CUDA else "cpu")
 
 def maskNLLLoss(inp, target, mask):
     nTotal = mask.sum()
-    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)))
+    crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
     loss = crossEntropy.masked_select(mask).mean()
     return loss, nTotal.item()
 
